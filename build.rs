791fdd012c4b6ab5421233e1f4fa87db3f1393bf
Tomyk <tomyk9991@web.de>
2024-12-01 15:10:49 +0100

    fixed rerun in build

--- build.rs
+++ build.rs
@@ -47,9 +47,9 @@ fn main() -> anyhow::Result<()> {{
     }
 
 
-    fs::write(mod_rs_path, mod_rs).expect("Unable to write to year2023/mod.rs");
+    fs::write(mod_rs_path, mod_rs).expect(&format!("Unable to write to year{year}/mod.rs"));
     fs::write(main_rs_path, main_rs).expect("Unable to write to main.rs");
 
-    println!("cargo:rerun-if-changed=./src/year2023");
+    println!(format!("cargo:rerun-if-changed=./src/year{year}"));
     println!("cargo:rerun-if-changed=build.rs");
 }
